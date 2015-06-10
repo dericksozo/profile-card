eae53e9701f83c0fbb7af2af0613cbb7534c2912
itsderick <derickiscool@gmail.com>
2015-06-10 17:42:24 -0500

    Making browser sync my default server because it's cooler. Setting up bourbon correctly with node-sass, turning on source maps for chrome developer tools and workspaces. Everything else should work as expected. Just sudo npm install everything and then run grunt in the cmd.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,65 +1,42 @@
-/*global module:false*/
-module.exports = function(grunt) {
-
-  // Project configuration.
-  grunt.initConfig({
-    // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-    sass: {
-      dev: {
-        src: ['src/scss/styles.scss'],
-        dest: 'src/css/styles.css',
-
-        options: {
-            loadPath: require('node-bourbon').includePaths
+// This shows a full config file!
+module.exports = function (grunt) {
+    grunt.initConfig({
+        watch: {
+            files: 'src/scss/**/*.scss',
+            tasks: ['sass']
+        },
+        sass: {
+            dev: {
+                files: {
+                    'src/css/styles.css': 'src/scss/styles.scss'
+                },
+                options: {
+                    includePaths: require('node-bourbon').includePaths,
+                    sourceMap: true
+                }
+            }
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        'src/css/*.css',
+                        'src/*.html'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: './src'
+                }
+            }
         }
-      },
-    },
+    });
 
-    watch: {
-      sass: {
-        // We watch and compile sass files as normal but don't live reload here
-        files: ['src/scss/**/*.scss'],
-        tasks: ['sass'],
-      },
-      livereload: {
-        // Here we watch the files the sass task will compile to
-        // These files are sent to the live reload server after sass compiles to them
-        options: { livereload: true },
-        files: ['src/css/*', 'src/js/*', 'src/*.html'],
-      },
-    },
+    // load npm tasks
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
 
-    express: {
-      all: {
-        options: {
-          port: 9000,
-          hostname: "0.0.0.0",
-          bases: ['src'], // Replace with the directory you want the files served from
-                             // Make sure you don't use `.` or `..` in the path as Express
-                             // is likely to return 403 Forbidden responses if you do
-                             // http://stackoverflow.com/questions/14594121/express-res-sendfile-throwing-forbidden-error
-          livereload: true
-        }
-      }
-    },
-});
-
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-express');
-
-
-  // Default task.
-  // grunt.registerTask('default', ['sass', 'express', 'watch'/* 'uncss', 'cssmin', 'uglify', 'processhtml', 'htmlmin' */]);
-  // grunt.registerTask('default', ['uncss', 'cssmin', 'uglify', 'concat', 'processhtml', 'htmlmin']);
-  // Start a server
-  grunt.registerTask('server', ['sass', 'express', 'watch']);
-
+    // define default task
+    grunt.registerTask('default', ['browserSync', 'watch']);
 };
